36460121b7be64bf9b5d9ec682829aebc83e4b7d
Pankaj Sharma <23135201@airtel.africa>
2023-01-30 15:44:50 +0530

    Added

--- src/test/java/com/qa/java/tests/FactorialNumber.java
+++ src/test/java/com/qa/java/tests/FactorialNumber.java
@@ -6,10 +6,10 @@ public class FactorialNumber {
 
 	 public static void main(String[] args) {
 	        int num;
-	        Scanner in = new Scanner(System.in);
-	        System.out.println("Enter your number");
-	        num = in.nextInt();
-	        
+	        try (Scanner in = new Scanner(System.in)) {
+				System.out.println("Enter your number");
+				num = in.nextInt();
+			}
 	        int fact=1;
 	        for(int i=1; i<=num; i++)
 	        {
