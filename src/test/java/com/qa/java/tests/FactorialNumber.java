4f6a611d9592acb222c3cd6edbde443628741580
Pankaj Sharma <23135201@airtel.africa>
2023-01-30 15:40:47 +0530

    Added factorial java program added

---
+++ src/test/java/com/qa/java/tests/FactorialNumber.java
@@ -0,0 +1,25 @@
+package com.qa.java.tests;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	 public static void main(String[] args) {
+	        int num;
+	        Scanner in = new Scanner(System.in);
+	        System.out.println("Enter your number");
+	        num = in.nextInt();
+	        
+	        int fact=1;
+	        for(int i=1; i<=num; i++)
+	        {
+	        	fact=fact*i;
+	        	
+	        }
+	        System.out.println(fact);	        	
+
+	         
+	    }
+	
+	
+}
