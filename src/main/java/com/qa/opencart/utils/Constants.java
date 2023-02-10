d38c89a33f1be40bfd8202264a7a055af2f625f6
Pankaj Sharma <23135201@airtel.africa>
2023-02-11 03:16:05 +0530

    Set header value null instaed My Store

--- src/main/java/com/qa/opencart/utils/Constants.java
+++ src/main/java/com/qa/opencart/utils/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	public static final String LOGIN_PAGE_TITLE = "Account Login";
 	public static final String LOGIN_PAGE_URL_FRACTION = "route=account/login";
 	public static final String ACCOUNT_PAGE_TITLE = "My Account";
-	public static final String ACCOUNT_PAGE_HEADER = "Your Store";
+	public static final String ACCOUNT_PAGE_HEADER = "";
 	public static final String PRODUCT_HEADER = "MacBook Pro";
 
 	public static final int IMAC_IMAGE_COUNT = 3;
